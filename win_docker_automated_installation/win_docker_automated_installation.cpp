d818c420a90b8f4b22e718852913fcd6f84c590b
Nightstars <ChristZhangowner@gmail..com>
2021-02-02 17:18:16 +0800

    添加项目文件。

---
+++ win_docker_automated_installation/win_docker_automated_installation.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+
+int main()
+{
+	typedef int(*FUNC)();
+	HINSTANCE hDllInst;
+	hDllInst = LoadLibrary(L"commander.dll");
+	FUNC autoInstall = (FUNC)GetProcAddress(hDllInst, "autoInstall");
+	int stat = autoInstall();
+	FreeLibrary(hDllInst);
+    return 0;
+}
